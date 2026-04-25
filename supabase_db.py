1524f0b5273f856d911c23877f80ec9dd6569a74
Divyanshu Madan <divmadan99@gmail.com>
2026-04-25 19:59:27 +0530

    db api rehaul

--- supabase_db.py
+++ supabase_db.py
@@ -3,81 +3,152 @@ import random
 from supabase import create_client, Client
 from dotenv import load_dotenv
 from functools import wraps
-from typing import Callable, Any
+from dataclasses import dataclass, asdict
 
 load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 
-def query_table(table_name: str, data_filter: Callable[[list], Any]):
+def execute(data_filter=None):
     def decorator(func):
         @wraps(func)
         def wrapper(self, *args, **kwargs):
-            table = self.supabase.table(table_name)
-            query = func(self, table, *args, **kwargs)
+            query = func(self, *args, **kwargs)
             data = query.execute().data
-            return data_filter(data) if data else None
+            return data_filter(data) if data and data_filter else data
 
         return wrapper
 
     return decorator
 
 
+@dataclass
+class DBActivityMetadata:
+    use_count: int = 0
+    vote_count: int = 0
+
+
 class DB:
     def __init__(self):
         self.supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-    @query_table("session", lambda data: data[0])
-    def create_session(self, table, msg_id: int):
-        return table.insert({"msg_id": msg_id})
+    def _table(self, table_name: str):
+        return self.supabase.table(table_name=table_name)
 
-    @query_table("session", lambda data: data[0]["msg_id"])
-    def get_session(self, table):
-        return table.select("msg_id, is_active").eq("is_active", True).order("created_at", desc=True).limit(1)
+    # -------------------- session --------------------
 
-    @query_table("session", lambda data: data[0])
-    def update_session_deactivate(self, table):
-        return table.update({"is_active": False}).eq("is_active", True)
+    @execute
+    def create_session(self, id: int):
+        return self._table("session").insert({"id": id}).execute()
 
-    @query_table("server", lambda data: data[0]["name"])
-    def get_server_id(self, table, name: str, kind: str):
-        return table.select("name, id_type").eq("name", name).eq("kind", kind)
+    @execute(lambda data: data[0]["id"])
+    def get_session(self):
+        return self._table("session").select("id, is_active").eq("is_active", True).order("created_at", desc=True).limit(1)
 
-    @query_table("session", lambda data: data[0]["activity"])
-    def get_session_activity(self, table):
-        return table.select("msg_id, is_active, activity").eq("is_active", True).order("created_at", desc=True).limit(1)
+    @execute
+    def deactivate_session(self):
+        return self._table("session").update({"is_active": False}).eq("is_active", True)
 
-    def get_session_active_members(self):
-        activity = self.get_session_activity()
-        members = set()
-        for entry in activity:
-            if entry["kind"] == "join":
-                members.add(entry["member_id"])
-            elif entry["kind"] == "leave":
-                members.remove(entry["member_id"])
-        return members
+    # -------------------- server --------------------
 
-    @query_table("excuses", lambda data: data[0])
-    def create_excuse(self, table, excuse: str, creator_id: int):
-        return table.insert({"excuse": excuse, "creator_id": creator_id})
+    @execute(lambda data: data[0]["id"])
+    def get_server_id(self, name: str, kind: str):
+        return self._table("server").select("id, name, kind").eq("name", name).eq("kind", kind)
 
-    @query_table("excuses", lambda data: random.choice(data)["excuse"])
-    def get_excuse(self, table):
-        return table.select("excuse")
+    # -------------------- create activity --------------------
 
-    @query_table("agenda", lambda data: data[0])
-    def create_agenda(self, table, agenda: str, creator_id: int, msg_id: int | None = None):
-        return table.insert({"agenda": agenda, "creator_id": creator_id, "msg_id": msg_id})
+    @execute
+    def _create_activity(self, kind: str, value: str, member_id: int, metadata: DBActivityMetadata | None = None):
+        if not metadata:
+            metadata = DBActivityMetadata()
 
-    @query_table("agenda", lambda data: data[0]["agenda"])
-    def get_agenda(self, table):
-        return table.select("agenda, is_active, created_at").eq("is_active", True).order("created_at", desc=True).limit(1)
+        return self._table("session").insert({"kind": kind, "value": value, "member_id": member_id, "metadata": asdict(metadata)})
 
-    @query_table("agenda", lambda data: data[0])
-    def update_agenda_deactivate(self, table):
-        return table.update({"is_active": False}).eq("is_active", True)
+    def add_agenda(self, agenda: str, member_id: int):
+        return self._create_activity("add_agenda", agenda, member_id)
 
-    @query_table("server", lambda data: data[0]["name"])
-    def get_server_id(self, table, name: str, id_type: str):
-        return table.select("name, id_type").eq("name", name).eq("id_type", id_type)
+    def add_excuse(self, excuse: str, member_id: int):
+        return self._create_activity("add_excuse", excuse, member_id)
+
+    def add_hallucination(self, hallucination: str, member_id: int):
+        return self._create_activity("add_hallucination", hallucination, member_id)
+
+    def join_call(self, member_id: int):
+        session_id = self.get_session()
+        return self._create_activity("join_call", session_id, member_id)
+
+    def leave_call(self, member_id: int):
+        session_id = self.get_session()
+        return self._create_activity("leave_call", session_id, member_id)
+
+    def pause_call(self, member_id: int):
+        session_id = self.get_session()
+        return self._create_activity("pause_call", session_id, member_id)
+
+    # -------------------- get activity --------------------
+
+    @execute
+    def _get_activity(self, kind: str, value: str):
+        return self._table("activity").select("*").eq("kind", kind).eq("value", value)
+
+    @execute(lambda data: data[0]["value"])
+    def get_latest_agenda(self):
+        return self._table("activity").select("kind, value").eq("kind", "add_agenda").order("created_at", desc=True).limit(1)
+
+    @execute(lambda data: random.choice(data)["value"])
+    def get_random_excuse(self):
+        return self._table("activity").select("kind, value").eq("kind", "add_excuse")
+
+    @execute(lambda data: random.choice(data)["value"])
+    def get_random_hallucination(self):
+        return self._table("activity").select("kind, value").eq("kind", "add_hallucination")
+
+    def get_active_members(self):
+        session_id = self.get_session()
+
+        join_call_data = self._get_activity("join_call", session_id)
+        joined = set(item["member_id"] for item in join_call_data)
+
+        left_call_data = self._get_activity("left_call", session_id)
+        left = set(item["member_id"] for item in left_call_data)
+
+        return list(joined.difference(left))
+
+    # -------------------- update activity metadata --------------------
+
+    @execute
+    def _update_metadata(self, kind: str, value: str, metadata: DBActivityMetadata):
+        return self._table("activity").update({"metadata": asdict(metadata)}).eq("kind", kind).eq("value", value)
+
+    def _use_activity(self, kind: str, value: str):
+        for item in self._get_activity(kind, value):
+            metadata = DBActivityMetadata(**item["metadata"])
+            metadata.use_count += 1
+
+            self._update_metadata(item["kind"], item["value"], metadata)
+
+    def _vote_activity(self, kind: str, value: str):
+        for item in self._get_activity(kind, value):
+            metadata = DBActivityMetadata(**item["metadata"])
+            metadata.vote_count += 1
+
+            self._update_metadata(item["kind"], item["value"], metadata)
+
+    def use_agenda(self, agenda: str):
+        return self._use_activity("add_agenda", agenda)
+
+    def use_excuse(self, excuse: str):
+        return self._use_activity("add_excuse", excuse)
+
+    def use_hallucination(self, hallucination: str):
+        return self._use_activity("add_hallucination", hallucination)
+
+    def vote_agenda(self, agenda: str):
+        return self._vote_activity("add_agenda", agenda)
+
+    def vote_excuse(self, excuse: str):
+        return self._vote_activity("add_excuse", excuse)
+
+    def vote_hallucination(self, hallucination: str):
+        return self._vote_activity("add_hallucination", hallucination)
